a59c650e116f69d062ea9807ab1aece01cc3be56
rsivaraja <riasivaraja@gmail.com>
2025-07-18 19:19:18 +0100

    Add files via upload

--- hot_potato.py
+++ hot_potato.py
@@ -1,16 +1,18 @@
 import random
+from queues import queue_actions
 print('Welcome to the hot potato game')
 print('The rules of the game are as the following: There is a hot potato and there are n passes and whoever ends up with the potato at the end is eliminated\nThe last one standing wins.  ')
-players=[]
+#players=[]
+players=queue_actions()
 nplayers=int(input('How many players are in the game?'))
 for i in range(nplayers):
     name=input('Enter the player name:')
-    players.append(name)
+    players.enqueue(name)
 passes=random.randint(1,10)
 print('We are passing the potato ', passes, ' times')
-while len(players)>1:
+while players.size()>1:
     for i in range(0,passes):
-        x=players.pop(0)
-        players.append(x)
-    eliminated=players.pop(0)
-print('The winner is',players.pop(0))
+        x=players.dequeue()
+        players.enqueue(x)
+    eliminated=players.dequeue()
+print('The winner is',players.dequeue())
